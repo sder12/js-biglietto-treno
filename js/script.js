a311e503a7d0e27e8aad1da9ac6a4f09a46f01bc
sder12 <sara.cetto@gmail.com>
2022-10-06 15:02:28 +0200

    Processing + Output

--- js/script.js
+++ js/script.js
@@ -15,13 +15,29 @@ const userKm = parseInt(prompt("Quanti km devi percorrere?"))
 console.log(userAge, typeof(userAge));
 console.log(userKm, typeof(userKm));
 
-
-//PROCESSING
 //generare il prezzo intero del biglietto (0.21 € al km)
+const fullCost = userKm * 0.21;
+console.log(fullCost, typeof(fullCost));
+
 // SE età < 18 applico sconto del 20%
 //ALTRIMNETI SE età >= 65 applico sconto del 40%
 // ALTRIMENTI non ci sarà nessuno sconto per età compresa tra 18 e 85
+const discount20 = (fullCost * 20) / 100;
+const discount40  = (fullCost * 40) / 100;
+console.log(discount20, discount40)
+
+const youngCost = fullCost - discount20;
+const oldCost = fullCost - discount40;
+console.log(youngCost, oldCost)
 
 //OUTPUT
 // arrotondo tutti i prezzi ai centesimi 0.00
 // alert Messaggio: Salve cliente,il prezzo del tuo biglietto é...
+const message ="Il prezzo del tuo biglietto è di "
+if (userAge < 18){
+    alert(message + youngCost)
+} else if (userAge >= 65){
+    alert(message + oldCost)
+} else{
+    alert(message + fullCost)
+}
